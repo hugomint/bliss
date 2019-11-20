6b4b861ae3cdf1212b9011cd6a80b0426b95048a
hugomint <57132886+hugomint@users.noreply.github.com>
2019-11-20 15:12:17 +1000

    fin

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import R from "ramda";
 
 import "./styles.css";
 
-function App() {
+console.log(R.add(2, 3));
+R.add(7)(10);
+
+function App(props) {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello CodeSandbox {props.message}</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
@@ -16,11 +19,11 @@ function App() {
 let dish = React.createElement("h1", { id: "recipe-0" }, "Baked Salmon");
 let text = React.createElement(
   "p",
-  { id: "recipe-1" },
+  { id: "recipe-1", key: "cool" },
   "Mix some Baked Salmon in a bowl and pray!"
 );
-ReactDOM.render([<App />, dish, text], document.getElementById("root"));
+ReactDOM.render(
+  [<App message="Dave" />, dish, text],
+  document.getElementById("root")
+);
 document.getElementById("root");
-
-console.log(R.add(2, 3));
-R.add(7)(10);
